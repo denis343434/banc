13fa7b69af8594c3244c08edda05fc699a369131
denis343434 <161150159+denis343434@users.noreply.github.com>
2024-05-13 20:08:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+$('#modl').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.text-battun').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.btn-programs').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.batton-table').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.pay-button').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.ham-menu-taitle').click(function(){
+	$('.ham-menu-1').toggleClass('d-n');
+});
+$('#menu-1').click(function(){
+	$('.ham-menu-1').toggleClass('d-n');
+});
+$('#menu').click(function(){
+	$('.hamburger').toggleClass('d-n');
+});
+$('.pay-button-1').click(function(){
+	$('.chart-container').toggleClass('d-n');
+});
+$('.close-chat-table').click(function(){
+	$('.chart-container').toggleClass('d-n');
+});
+function scrollToElement() {
+  var element = document.getElementById('scrollToMe');
+  element.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToElement() {
+  var element = document.getElementById('scrollToMe1');
+  element.scrollIntoView({ behavior: 'smooth' });
+}
